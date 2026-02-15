4dd6379d12ef47b9a79a1f39212fad9bc63876b4
wcho <chotikawimaro@Magic-Macbook.local>
2026-02-15 23:27:46 +0700

    [patch] refactor: implement custom decorator

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './interfaces/jwt.interface';
+import { ActiveUser } from './interfaces/active-user.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: JwtPayload) {
+  validate(payload: JwtPayload): ActiveUser {
     return { userId: payload.sub, username: payload.username };
   }
 }
